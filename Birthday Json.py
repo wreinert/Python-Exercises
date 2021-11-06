b1c367a5aae37ee48805c2ea27dc4bf7e33aa81f
William Reinert <williamreinert@hotmail.com>
2021-11-06 12:00:33 -0300

    Birthday Json

--- Birthday Json.py
+++ Birthday Json.py
@@ -2,13 +2,14 @@
 
 # Bonus: Ask the user for another scientist’s name and birthday to add to the dictionary, and update the JSON file you have on disk with the scientist’s name. If you run the program multiple times and keep adding new names, your JSON file should keep getting bigger and bigger.
 
+# Birthday search
 import json
 
 print("Welcome to the birthday dictionary. We know the birthdays of:")
 with open("birthdays.json", "r") as f:
     scientists = json.load(f)
 for i in scientists:
-    print(i)    
+    print(i)
 
 user_pick = input("Enter a name: ")
 new_scientist = input("Now enter a new scientist's name: ")
@@ -18,8 +19,4 @@ scientists[new_scientist] = birth_date
 with open("birthdays.json", "w") as w:
     json.dump(scientists, w)
 
-print(
-    "{}'s birthday is {}.".format(
-        user_pick.capitalize(), (scientists.get(user_pick.capitalize(), "not found"))
-    )
-)
+print("{}'s birthday is {}.".format(user_pick.title(), (scientists.get(user_pick.title(), "not found"))))
