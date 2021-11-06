0afe881a9931612d6030161c451697b20cd59b43
William Reinert <williamreinert@hotmail.com>
2021-11-05 23:36:24 -0300

    Adjustments

--- Birthday Json.py
+++ Birthday Json.py
@@ -1,19 +1,25 @@
-#Modify your program from Part 1 to load the birthday dictionary from a JSON file on disk, rather than having the dictionary defined in the program.
+# Modify your program from Part 1 to load the birthday dictionary from a JSON file on disk, rather than having the dictionary defined in the program.
 
-#Bonus: Ask the user for another scientist’s name and birthday to add to the dictionary, and update the JSON file you have on disk with the scientist’s name. If you run the program multiple times and keep adding new names, your JSON file should keep getting bigger and bigger.
+# Bonus: Ask the user for another scientist’s name and birthday to add to the dictionary, and update the JSON file you have on disk with the scientist’s name. If you run the program multiple times and keep adding new names, your JSON file should keep getting bigger and bigger.
 
-birthdays = {"Will": "01/03/2021", "Bill": "02/03/2021"}
-names = []
+import json
 
 print("Welcome to the birthday dictionary. We know the birthdays of:")
-for i in birthdays:
-    print(i)
+with open("birthdays.json", "r") as f:
+    scientists = json.load(f)
+for i in scientists:
+    print(i)    
 
 user_pick = input("Enter a name: ")
+new_scientist = input("Now enter a new scientist's name: ")
+birth_date = input("Now enter their birth date: ")
+scientists[new_scientist] = birth_date
 
+with open("birthdays.json", "w") as w:
+    json.dump(scientists, w)
 
 print(
     "{}'s birthday is {}.".format(
-        user_pick.capitalize(), (birthdays.get(user_pick.capitalize(), "not found"))
+        user_pick.capitalize(), (scientists.get(user_pick.capitalize(), "not found"))
     )
 )
